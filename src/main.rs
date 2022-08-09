ca475e3725006e18363179c33df2e846a4806287
spidrteam <findmymmcrypto@gmail.com>
2022-08-09 14:43:53 +0800

    Commented version of rust crash course
    https://www.youtube.com/watch?v=-AAtfPHEMbA

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     // let unsigned: u8 = 10;
     // // let signed: i8 = -10;
@@ -26,17 +28,64 @@ fn main() {
     // let slice = &arr[1 .. 3]; // sub-array, don't know the length
     // borrowing_slice(arr, slice);
 
-    // String
-    let str: &str = "hello world";
-    let mut string: String = String::from("Hello World");
+    // // String
+    // let str: &str = "hello world";
+    // let mut string: String = String::from("Hello World");
 
-    let slice = &string[.. 6];
-    println!("{}", slice.len());
+    // let slice = &string[.. 6];
+    // println!("{}", slice.len());
 
-    string.push('1');
-    string.push_str("! Bob");
-    string = string.replace("Hello", "Bye");
-    println!("{}", string);
+    // string.push('1');
+    // string.push_str("! Bob");
+    // string = string.replace("Hello", "Bye");
+    // println!("{}", string);
+
+    // // match statement
+    // let i = 4;
+    // match i {
+    //     0 => println!("0"),
+    //     1 | 2 => println!("1,2"),
+    //     3..=5 => println!("3,4,5"),
+    //     _ => println!("default")
+    // }
+
+    // // Struct
+    // let name = String::from("Farfetch");
+    // let bird = Bird {name: name, attack: 5};
+    // bird.print_name();
+    // println!("{} {}", bird.can_fly(), bird.is_animal());
+
+    // // maps
+    // let mut map = HashMap::new();
+
+    // map.insert(0, "Hi");
+    // map.insert(1, "Hi2");
+    // println!("{:?}", map);
+
+    // match map.get(&2) {
+    //     Some(str) => println!("{}", str),
+    //     _ => println!("Doesn't Exist.")
+    // }
+
+    // map.remove(&0);
+    // println!("{:?}", map);
+
+    // // Options
+    // let divide1: Option<i32> = divide(4, 2);
+    // let divide2: Option<i32> = divide(2, 3);
+
+    // println!("{:?} unwraps to {}", divide1, divide1.unwrap());
+    // println!("{:?} unwraps to {}", divide2, divide2.unwrap());
+
+    // // Result
+    // let divide = divide(4, 2);
+    // // let res = divide.expect("Crashed");
+
+    // if divide.is_ok() {
+    //     println!("{}", divide.unwrap());
+    // }
+    // // println!("{}", divide.unwrap_or(100));
+    // // println!("{}", res);
 }
 
 // pub fn is_even(num: u8) -> bool {
@@ -49,4 +98,55 @@ fn main() {
 //     println!("{:?}", slice);
 //     println!("length: {}", slice.len());
 //     println!("{} {}", slice[0], slice[1]);
+// }
+
+// // struct
+// struct Bird {
+//     name: String,
+//     attack: u64
+// }
+
+// impl Bird {
+//     fn print_name(&self) {
+//         println!("{} {}", self.name, self.attack);
+//     }
+// }
+
+// impl Animal for Bird {
+//     fn can_fly(&self) -> bool {
+//         true
+//     }
+//     fn is_animal(&self) -> bool {
+//         false
+//     }
+// }
+
+// trait Animal {
+//     fn can_fly(&self) -> bool;
+//     fn is_animal(&self) -> bool {
+//         true
+//     }
+// }
+
+// // Options
+// fn divide(dividend: i32, divisor: i32) -> Option<i32> {
+//     if dividend % divisor != 0 {
+//         None
+//     } else {
+//         Some (dividend / divisor)
+//     }
+// }
+
+// // Results
+// #[derive(Debug)]
+// enum MyError {
+//     Error1
+// }
+
+// fn divide(dividend: i32, divisor: i32) -> Result<i32,MyError> {
+//     if dividend % divisor != 0 {
+//         Err(MyError::Error1)
+//     } else {
+//         Ok(dividend / divisor)
+//     }
 // }
